42128ba616e7ceb0e2e59a366ee2ccc51fb99e06
HarithaBal <66410023+HarithaBal@users.noreply.github.com>
2021-11-30 19:31:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-res.send("<h1>Hello from nodejs application, From inside Doker container...</h1>");
+res.send("<h1>Hello from nodejs build by app2, From inside Doker container...</h1>");
 });
 
 app.listen(3000, ()=> {
